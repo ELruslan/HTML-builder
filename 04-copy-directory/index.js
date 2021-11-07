94ef707d854f9a73ba429630413d89cb976c686e
elruslan <eloyan666@gmail.com>
2021-11-07 22:15:49 +0300

    replace index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,14 +1,51 @@
 var fs = require('fs');
 
-fs.mkdir('04-copy-directory/files-copy',function (err){
-  if (err) {
-    console.error(err);}
-});
-const testFolder = '04-copy-directory/files/';
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    fs.createReadStream('04-copy-directory/files/'+`${file}`).pipe(fs.createWriteStream('04-copy-directory/files-copy/'+`${file}`));
-    console.log(file);
+
+
+ 
+
+fs.stat('04-copy-directory/files-copy', function(err) {
+  if (!err) {
+      fs.readdir('04-copy-directory/files-copy',function(err,files){
+        if (err)
+        console.log(err);
+        files.forEach(file=>{
+          fs.unlink('04-copy-directory/files-copy/'+`${file}`,function(err){
+            if(err) return console.log(err);
+          });
+          
+      })
+      fs.rmdir('04-copy-directory/files-copy',function(err){
+        if (err) {
+          return console.error(err);
+      }
+      })
+  })
+}  else if (err.code === 'ENOENT') {
+    fs.mkdir('04-copy-directory/files-copy/',function (err){
+      if (err) {
+        console.error(err);} 
+        fs.readdir('04-copy-directory/files-copy/',function(err){
+          if (err)
+          console.log(err);
+          const testFolder = '04-copy-directory/files/';
+          fs.readdir(testFolder, (err, files) => {
+            if (err)
+            console.log(err);
+            files.forEach(file => {
+              fs.createReadStream('04-copy-directory/files/'+`${file}`).pipe(fs.createWriteStream('04-copy-directory/files-copy/'+`${file}`));
+            });
+          });
+        })
   });
+     
+  }
 });
+
+
+
+
+
+
+
 
